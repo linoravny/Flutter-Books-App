e46ff8ed02f6b032a364ff438183421f71c0ce7c
Linor <linoravny@gmail.com>
2021-04-06 15:26:14 +0300

    provider fixes

--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -1,27 +1,37 @@
+import 'package:books_app/src/business_logic/models/user.dart';
 import 'package:books_app/src/views/pages/books_categories.dart';
 import 'package:books_app/src/views/pages/home.dart';
 import 'package:books_app/src/views/pages/login_form.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'business_logic/providers/user_provider.dart';
 
 class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final userData = Provider.of<UserData>(
+      context,
+    ).userData;
+    final isUserAuth = (userData != null) ? userData.isAuth : false;
+
     return Drawer(
       child: ListView(
         children: <Widget>[
           DrawerHeader(
             child: Text("app menu"),
           ),
-          ListTile(
-            leading: Icon(Icons.login, size: 18),
-            title: Text('Login'),
-            onTap: () => {
-              Navigator.pushNamed(
-                context,
-                LoginForm.routeName,
-              ),
-            },
-          ),
+          if (!isUserAuth)
+            (ListTile(
+              leading: Icon(Icons.login, size: 18),
+              title: Text('Login'),
+              onTap: () => {
+                Navigator.pushNamed(
+                  context,
+                  LoginForm.routeName,
+                ),
+              },
+            )),
           ListTile(
             leading: Icon(Icons.home),
             title: Text('Home'),
@@ -42,6 +52,17 @@ class Menu extends StatelessWidget {
               )
             },
           ),
+          if (isUserAuth)
+            (ListTile(
+              leading: Icon(Icons.logout, size: 18),
+              title: Text('Logout'),
+              onTap: () => {
+                Provider.of<UserData>(
+                  context,
+                  listen: false,
+                ).setUserData(null)
+              },
+            )),
         ],
       ),
     );
